f87a5ea9c17bb5c3d3f0f515ba5ac86f869c301b
hannah68 <reihaneh.naderi1989@gmail.com>
2021-11-19 11:38:02 +0000

    creating home page

---
+++ src/scripts/app.js
@@ -0,0 +1,48 @@
+const main = document.querySelector('main');
+const homeSection = document.createElement('div');
+homeSection.classList.add('home-section');
+main.append(homeSection);
+
+const heroSectionFn = () => {
+    const heroSection = document.createElement('section');
+    heroSection.classList.add('hero-section');
+    homeSection.append(heroSection);
+    const heroText = document.createElement('div');
+    heroText.classList.add('hero-section__text');
+    const heroContainer = document.createElement('div');
+    heroContainer.classList.add('hero-container');
+    heroText.append(heroContainer);
+    const heroTitle = document.createElement('h1');
+    heroTitle.innerText = 'The unique furniture for your special house';
+    heroTitle.classList.add('hero-section__text--title');
+    const heroBtn = document.createElement('button');
+    heroBtn.innerText = 'Shop Now';
+    heroBtn.classList.add('hero-section__text--btn');
+    heroContainer.append(heroTitle, heroBtn)
+    const heroImage = document.createElement('div');
+    heroImage.classList.add('hero-section__img');
+    const heroImg = document.createElement('img');
+    heroImg.src = './assets/images/chair1.jpg';
+    heroImg.setAttribute('alt', 'chair image');
+    heroImage.append(heroImg);
+    heroSection.append(heroText, heroImage);
+}
+
+
+
+const featuresSectionFn = () => {
+    const featureSection = document.createElement('section');
+    featureSection.classList.add('features-section');
+    const h2El = document.createElement('h2');
+    h2El.innerText = 'Best selling furniture';
+    const featureContainer = document.createElement('div');
+    featureContainer.classList.add('features-container');
+    featureSection.append(h2El, featureContainer);
+    const featureItem = document.createElement('div');
+    featureItem.classList.add('features-item');
+    featureContainer.append(featureItem);
+
+}
+
+heroSectionFn();
+featuresSectionFn();
